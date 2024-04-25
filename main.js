3ff0cc8d79651d963efb2a98c218e4ed6259ec1a
DoVanThuan <dovanthuan0902@gmail.com>
2024-04-25 11:55:00 +0700

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+// var makeColor = document.querySelectorAll('.nav-link');
+// for(var i =0;i<makeColor.length;++i){
+//     makeColor[i].style.color =' black'
+// }
+
+// var boxHiden =document.querySelector('.boxHiden');
+// var h1 = document.querySelector('button')
+
+
+// function Close(){
+//     return boxHiden.classList.remove('close')
+// }
+// function Open(){
+//     return boxHiden.classList.add('close')
+// }
+
+function btn(){
+    var imgMou = document.querySelectorAll('imgMou>img');
+    console.log(imgMou)
+
+};  
+
+var a =1
+console.log(a)
+
+
