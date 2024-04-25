38add318c5209d3c2228882dc9ecca7ddbab60da
DoVanThuan <dovanthuan0902@gmail.com>
2024-04-25 10:09:53 +0700

    fist commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var makeColor = document.querySelectorAll('.nav-link');
+for(var i =0;i<makeColor.length;++i){
+    makeColor[i].style.color =' black'
+}
+
+var boxHiden =document.querySelector('.boxHiden');
+var h1 = document.querySelector('button')
+
+
+function Close(){
+    return boxHiden.classList.remove('close')
+}
+function Open(){
+    return boxHiden.classList.add('close')
+}
+
